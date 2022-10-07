4c0019c07322f8a92439469c8adf94e4b026e5a4
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-10-07 05:10:45 -0500

    Biomodal component

---
+++ resources/js/Components/BioModal.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onUpdated } from '@vue/runtime-core'
+    const props = defineProps({
+        isShowing: Boolean,
+    })
+    onUpdated(()=>{
+        console.log(
+
+            props.isShowing
+        );
+    })
+    
+</script>
+<template>
+    <div id="bg-modal" class="fixed top-0 inset-0 bg-zinc-700/60 flex justify-center items-center" v-show="isShowing" @click="$emit('close')">
+        <div id="card" class="w-full md:w-[80vw] md:min-w-[80vw] opacity-full " @click.stop="">
+
+            <slot>
+                
+            </slot>
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
