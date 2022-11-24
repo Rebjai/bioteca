d6d2fb18a6eb3b342de065df7c2863149c35217d
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-11-24 04:45:11 -0600

    disable query log

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \DB::disableQueryLog();
         $this->call(
             [
                 UserSeeder::class,
