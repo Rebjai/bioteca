94f4ce107c74142d06664238b90094611623c200
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-11-24 17:21:33 -0600

    memory limit fix (finally)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        ini_set('memory_limit', '-1');
+
         // \DB::disableQueryLog();
         $this->call(
             [
@@ -40,7 +42,7 @@ class DatabaseSeeder extends Seeder
             [
                 StateSeeder::class,
                 MunicipalitySeeder::class,
-                // LocationSeeder::class,
+                LocationSeeder::class,
                 // LocationSeeder2::class,
                 // LocationSeeder3::class,
                 // LocationSeeder4::class,
