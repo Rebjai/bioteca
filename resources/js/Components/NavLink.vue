5805a7c9321394eddd0d901228b26ed224e62455
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-11-28 21:53:06 -0600

    navlink style

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -5,8 +5,8 @@ import { Link } from '@inertiajs/inertia-vue3';
 const props = defineProps(['href', 'active']);
 
 const classes = computed(() => props.active
-    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-gray-500 text-sm font-medium leading-5 text-zinc-100 focus:outline-none focus:border-indigo-700 transition  duration-150 ease-in-out'
-    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-zinc-500 hover:text-zinc-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-gray-700 text-sm font-bold leading-5 text-zinc-100 focus:outline-none focus:border-indigo-700 transition  duration-150 ease-in-out'
+    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-zinc-800 hover:text-zinc-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
 );
 </script>
 
